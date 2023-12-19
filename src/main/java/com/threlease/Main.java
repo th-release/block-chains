acb60ac141487cbe8ffb0548031a4c47dd203a3e
th-release <cth.release@gmail.com>
2023-12-19 17:07:34 +0900

    feat: tcp p2p

--- src/main/java/com/threlease/Main.java
+++ src/main/java/com/threlease/Main.java
@@ -1,21 +1,34 @@
 package com.threlease;
 
-import com.threlease.blockchain.Block;
-import com.threlease.blockchain.Chain;
+import com.threlease.server.ClientHandler;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.security.NoSuchAlgorithmException;
-import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws NoSuchAlgorithmException {
-        Chain chain = new Chain();
-        chain.addBlock(List.of("Block #1"));
-        System.out.println(chain.getChain());
+    private static final int PORT = 6666;
+    public static void main(String[] args) throws IOException {
+        try (ServerSocket serverSocket = new ServerSocket(PORT)){
+            System.out.println("서버가 " + PORT + " 포트에서 수신 대기 중입니다...");
+            while (true) {
+                try {
+                    // 클라이언트 연결을 기다립니다.
+                    Socket clientSocket = serverSocket.accept();
+                    // 각 클라이언트 연결에 대해 새로운 스레드를 시작합니다.
+                    new Thread(new ClientHandler(clientSocket)).start();
+                } catch (IOException e) {
+                    System.out.println("클라이언트 연결 수₩락 중 예외 발생: " + e.getMessage());
+                    // 여기서 에러를 핸들링하고 루프를 계속합니다.
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("서버 소켓을 열 수 없습니다: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+//        Chain chain = new Chain();
+//        chain.addBlock(List.of("Block #01"));
+//        System.out.println(chain.getChain());
     }
 }
--- src/main/java/com/threlease/server/ClientHandler.java
+++ src/main/java/com/threlease/server/ClientHandler.java
@@ -1,2 +1,37 @@
-package com.threlease.server;public class ClientHandler {
+package com.threlease.server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ClientHandler implements Runnable {
+    private final Socket clientSocket;
+
+    public ClientHandler(Socket socket) {
+        this.clientSocket = socket;
+    }
+
+    @Override
+    public void run() {
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)) {
+
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println("클라이언트 (" + clientSocket.getInetAddress().getHostAddress() + "): " + inputLine);
+//                TODO: 소켓 관련 소스코드 작성
+                out.println("서버 응답: " + inputLine);
+            }
+        } catch (IOException e) {
+            System.out.println("클라이언트와의 통신 중 예외가 발생했습니다: " + e.getMessage());
+        } finally {
+            try {
+                clientSocket.close(); // 클라이언트 소켓을 닫습니다.
+            } catch (IOException e) {
+                System.out.println("클라이언트 소켓을 닫는 중 오류 발생: " + e.getMessage());
+            }
+        }
+    }
 }
