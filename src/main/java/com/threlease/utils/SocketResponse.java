eb1702bb0e655ed7148a2b65356da7a43325eb4a
th-release <cth.release@gmail.com>
2023-12-20 09:00:18 +0900

    feat: Created SocketResponse

--- src/main/java/com/threlease/utils/SocketResponse.java
+++ src/main/java/com/threlease/utils/SocketResponse.java
@@ -1,2 +1,25 @@
-package com.threlease.utils;public class SocketResponse {
+package com.threlease.utils;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Optional;
+
+@Getter
+@Setter
+@Builder(builderClassName = "SocketResponseBuilder")
+public class SocketResponse {
+    private boolean success;
+    private Optional<String> message;
+    private Optional<Object> data;
+
+    @Override
+    public String toString() {
+        return "{\n" +
+                "\"success\": " + success + ",\n" +
+                "\"message\":" + message.map(Object::toString).orElse(null) + ",\n" +
+                "\"data\":" + data.map(Object::toString).orElse(null) + "\n"+
+                '}';
+    }
 }
